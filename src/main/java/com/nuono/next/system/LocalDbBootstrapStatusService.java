5ac6f039cdec50823e9a8fc90205219761a8eed6
Admin <13021976+sggr_1_0@user.noreply.gitee.com>
2026-06-05 21:41:15 +0800

    chore: align schema migrations with master

--- src/main/java/com/nuono/next/system/LocalDbBootstrapStatusService.java
+++ src/main/java/com/nuono/next/system/LocalDbBootstrapStatusService.java
@@ -91,8 +91,8 @@ public class LocalDbBootstrapStatusService {
                 "classpath:db/init/074_product_site_offer_listing_started_at.sql",
                 "classpath:db/init/089_product_variant_spec.sql",
                 "classpath:db/init/079_product_site_offer_data_missing_site_coverage.sql",
-                "classpath:db/init/080_product_listing_dry_run.sql",
-                "classpath:db/init/081_product_listing_menu_permission.sql"
+                "classpath:db/init/090_product_listing_dry_run.sql",
+                "classpath:db/init/091_product_listing_menu_permission.sql"
             ));
         return payload;
     }
