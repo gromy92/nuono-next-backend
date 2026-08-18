d5d1ed104b8c91b17adf92b84304eb477bf9f17a
Admin <13021976+sggr_1_0@user.noreply.gitee.com>
2026-08-18 09:06:49 +0800

    test(schema): register migration 254 in catalog contract

--- scripts/tests/test_repository_migration_catalog.py
+++ scripts/tests/test_repository_migration_catalog.py
@@ -66,6 +66,7 @@ class RepositoryMigrationCatalogTest(unittest.TestCase):
                 "251_noon_auth_owner_scope_successor.sql",
                 "252_official_warehouse_asn_preflight_audit.sql",
                 "253_operations_competitor_search_result_retention.sql",
+                "254_procurement_ali1688_order_canonical_index.sql",
             ],
             [migration.key for migration in migrations],
         )
@@ -77,7 +78,7 @@ class RepositoryMigrationCatalogTest(unittest.TestCase):
                 "MANAGED", "MANAGED", "MANAGED", "AUTO_ADDITIVE", "AUTO_ADDITIVE",
                 "AUTO_ADDITIVE", "AUTO_ADDITIVE", "AUTO_ADDITIVE", "AUTO_ADDITIVE",
                 "AUTO_ADDITIVE", "AUTO_ADDITIVE", "MANAGED", "AUTO_ADDITIVE",
-                "AUTO_ADDITIVE",
+                "AUTO_ADDITIVE", "AUTO_ADDITIVE",
             ],
             [migration.kind for migration in migrations],
         )
